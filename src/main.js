743c7030839644635f2d4792f9a8e909157fc248
imvenx <imvenx@proton.me>
2025-08-06 16:13:44 +0200

    add memory

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const { ChatOllama } = require('@langchain/ollama');
 const { createReactAgent } = require("@langchain/langgraph/prebuilt");
+const { MemorySaver } = require("@langchain/langgraph");
+const { RunnableConfig } = require("@langchain/langgraph");
 const app = express();
 const PORT = 3000;
 
@@ -12,10 +14,15 @@ app.listen(PORT, () => console.log(`App started at http://localhost:${PORT}`))
 app.get('/', (req, res) => { res.sendFile(__dirname + '/frontend/index.html'); });
 
 const llm = new ChatOllama({ baseUrl: 'http://localhost:11434', model: 'gemma3:latest', temperature: 0.1 });
-const agent = createReactAgent({ llm: llm, tools: [], prompt: 'say hello' });
+const checkpointer = new MemorySaver();
+const agent = createReactAgent({ llm: llm, tools: [], prompt: 'aid the user', checkpointer });
+const config = { configurable: { thread_id: '1' } };
+
 async function chat() {
-    const answer = await agent.invoke({ message: 'hello' })
+    const answer = await agent.invoke({ messages: ['3'] }, config)
     console.log(answer)
+    const answer2 = await agent.invoke({ messages: ['+ 5?'] }, config)
+    console.log(answer2)
 }
 
 chat()
